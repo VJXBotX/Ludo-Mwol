8009ca216b7c69d183c2f232f5ffae45b92a112c
ɪᴛ's ᴍᴇ ᴊᴀɪɴ ᴠᴊ <90563322+VJXBotX@users.noreply.github.com>
2021-09-14 02:33:01 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -11,15 +11,15 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     
     
    
-  r_text[0] = "PicsArt_08-15-03.05.52.jpg";
+  r_text[0] = "https://www.pixelstalk.net/wp-content/uploads/2016/03/Pikachu-wallpaper-HD-dowload.jpg";
     
     
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*My Creator Is JaiN VJ*
-*Creator number : wa.me/916282809372?text=Hi%20JaiN%20VJ.%20*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*My creator is JaiN VJ*
+*Creator number : wa.me/916282809372?text=Hi%20Ameer%20Suhail.%20*
 *Bot setting video : https://youtu.be/_D4ZYuUSXjs*
 *Githublink (Setup)  :    https://github.com/VJXBotX/Ludo-Mwol*
 *Audio commads :   https://github.com/VJXBotX/Ludo-Mwol/tree/master/uploads*
