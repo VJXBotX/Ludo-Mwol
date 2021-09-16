e1827fa8e954b20af24c9a778c58a162c419ed1d
ɪᴛ's ᴍᴇ ᴊᴀɪɴ ᴠᴊ <90563322+VJXBotX@users.noreply.github.com>
2021-09-16 22:46:44 +0530

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -10,132 +10,132 @@ Asena.addCommand({pattern: 'menu', fromMe: false, desc: Lang.WP}, (async (messag
     var r_text = new Array ();
     
     
-    r_text[0] = "https://www.pixelstalk.net/wp-content/uploads/2016/03/Pikachu-wallpaper-HD-dowload.jpg";
+    r_text[0] = "PicsArt_08-15-03.05.52.jpg";
     
     
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `⛦━━━━━✨️Ameer Suhail✨️━━━━━⛦
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `⛦━━━━━✨️JaiN VJ✨️━━━━━⛦
 
 
-𝐇𝐞𝐥𝐥𝐨👋 𝐈 𝐚𝐦 𝐚 Ameer Suhail 𝐛𝐨𝐭.
-            *Pikachu*
+𝐇𝐞𝐥𝐥𝐨👋 𝐈 𝐀𝐦 𝐀 JaiN VJ 𝐁𝐨𝐭.
+            *𝙇𝙪𝙙𝙤-𝙈𝙬𝙤𝙡*
 
 ■□■□■□■□■□■□■□■□■□■□
 ♕ *𝙈𝙚𝙙𝙞𝙖 𝘾𝙤𝙢𝙢𝙖𝙣𝙙𝙨* ♕
 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -song <text>
+➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ .song <text>
 🍙𝙐𝙨𝙚➜ Downloads song for you.
 ⚠️.song  baby love
 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -video <yt link>
+➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ .video <yt link>
 🍙𝙐𝙨𝙚➜ Downloads video from YouTube link.
 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -insta <link>
+➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  .insta <link>
 🍙𝙐𝙨𝙚➜   Downloads content from instagram.
 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -yt<text>
+➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  .yt<text>
 🍙𝙐𝙨𝙚➜   Gives you YT links.
 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -show <show name>
+➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  .show <show name>
 🍙𝙐𝙨𝙚➜   Get info related to tv series and shows.
 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -gif 
+➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ .gif 
 🍙𝙐𝙨𝙚➜ Converts video to gif.
 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -tomp3 
+➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ .tomp3 
 🍙𝙐𝙨𝙚➜ Converts video into audio.
 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -say <text>
+➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ .tts <text>
 🍙𝙐𝙨𝙚➜ Converts text into voice.
 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -img <text>
+➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ .img <text>
 🍙𝙐𝙨𝙚➜ It sends image from google.
 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -trt <language code>
+➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ .trt <language code>
 🍙𝙐𝙨𝙚➜ Translate the text you tag.
 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -wiki <text>
+➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ .wiki <text>
 🍙𝙐𝙨𝙚➜ It sends Wikipedia result.
 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -lyric <text>
+➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ .lyric <text>
 🍙𝙐𝙨𝙚➜ Finds the lyrics of the song.
 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -covid <country code>
+➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ .covid <country code>
 🍙𝙐𝙨𝙚➜ Send the covid stats of your country.
 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -weather <city>
+➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ .weather <city>
 🍙𝙐𝙨𝙚➜ Tells you about the weather of your place.
 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -removebg 
+➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ .removebg 
 🍙𝙐𝙨𝙚➜ Removes the background of tge image.
 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -ocr
+➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ .ocr
 🍙𝙐𝙨𝙚➜ Finds the text written on the image.
 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -wallpaper
+➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ .wallpaper
 🍙𝙐𝙨𝙚➜ It sends you random wallpaper.
 
-■□■□■□■□■□Pikachu■□■□■□■□■□
+■□■□■□■□■□𝙡𝙪𝙙𝙤-𝙈𝙬𝙤𝙡■□■□■□■□■□
 ♟ *𝙁𝙪𝙣 𝘾𝙤𝙢𝙢𝙖𝙣𝙙𝙨* ♟
 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -joke 
+➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ .joke 
 🍙𝙐𝙨𝙚➜ It sends a random joke.
  
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -meme <text>
+➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ .meme <text>
 🍙𝙐𝙨𝙚➜ Cations the image into a meme.
 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -quote 
+➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ .quote 
 🍙𝙐𝙨𝙚➜ It sends a random quote.
 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -ss <website link>
+➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ .ss <website link>
 🍙𝙐𝙨𝙚➜ It sends the screenshot of the website.
 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -changesay <text>
+➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ .changesay <text>
 🍙𝙐𝙨𝙚➜ Converts text into changesay meme image.
 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -trumpsay
+➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ .trumpsay
 🍙𝙐𝙨𝙚➜ Convert text into Trump's tweet.
 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -compliment 
+➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ .compliment 
 🍙𝙐𝙨𝙚➜ Gives you a compliment.
 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -bitly <link>
+➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  .bitly <link>
 🍙𝙐𝙨𝙚➜   Shorten your link.
 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -dict 
+➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  .dict 
 🍙𝙐𝙨𝙚➜   Dictionary [-dict en;anime]
 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -zodiac <leo> 
+➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  .zodiac <leo> 
 🍙𝙐𝙨𝙚➜   Tells you about your horoscope.
 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -qr <text>
+➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  .qr <text>
 🍙𝙐𝙨𝙚➜   Converts text into qr code.
 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -movie 
+➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  .movie 
 🍙𝙐𝙨𝙚➜  Gives you info about movie.
 ⚠️movie master
 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  -anime <text>
+➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜  .anime <text>
 🍙𝙐𝙨𝙚➜  Gives you info about anime.
-⚠️anime pikachu
+⚠️anime LUDO-MWOL
 ■□■□■□■□■Pikachu□■□■□■□■□■□
 ♝ *𝙎𝙩𝙞𝙘𝙠𝙚𝙧 𝘾𝙤𝙢𝙢𝙖𝙣𝙙𝙨* ♝
 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -sticker 
+➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ .sticker 
 🍙𝙐𝙨𝙚➜ Converts img/gif into a sticker.
 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -photo 
+➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ .photo 
 🍙𝙐𝙨𝙚➜ Converts sticker into image.
 
-➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ -attp <text>
+➡️𝘾𝙤𝙢𝙢𝙖𝙣𝙙➜ .attp <text>
 🍙𝙐𝙨𝙚➜ Converts text into glowing sticker.
-⚠️ex  attp pikachu
+⚠️ex  attp Ludo-Mwol
 ■□■□■□■□■□■□■□■□■□■□
-═════💢Pikachu💢═════
-▣▣▣▣▣▣▣▣▣ameersuhail▣▣▣▣▣▣▣▣▣▣▣
+═════💢𝙇𝙪𝙙𝙤-𝙈𝙬𝙤𝙡💢═════
+▣▣▣▣▣▣▣▣▣JaiN VJ▣▣▣▣▣▣▣▣▣▣▣
 
 `}) 
 
